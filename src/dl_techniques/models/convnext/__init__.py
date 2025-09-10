3f39c804735b8cc77b1526414fb73b5e92ef5399
Nikolas Markou <nikolasmarkou@gmail.com>
2025-09-10 16:57:16 +0300

    [convnext] updated init

---
+++ src/dl_techniques/models/convnext/__init__.py
@@ -0,0 +1,9 @@
+from .convnext_v1 import ConvNeXtV1, create_convnext_v1
+from .convnext_v2 import ConvNeXtV2, create_convnext_v2
+
+__all__ = [
+    ConvNeXtV1,
+    ConvNeXtV2,
+    create_convnext_v1,
+    create_convnext_v2
+]
