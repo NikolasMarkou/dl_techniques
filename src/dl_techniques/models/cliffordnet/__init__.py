78c9a79711e72f6b486efe3dcc214d2825c1b102
Nikolas Markou <nikolasmarkou@gmail.com>
2026-05-04 17:12:58 +0300

    [iter-1/step-2] Export CliffordNetLMRouting from cliffordnet package

--- src/dl_techniques/models/cliffordnet/__init__.py
+++ src/dl_techniques/models/cliffordnet/__init__.py
@@ -1,5 +1,6 @@
 from .model import CliffordNet
 from .lm import CliffordNetLM
+from .lm_routing import CliffordNetLMRouting
 from .clip import CliffordCLIP
 from .unet import CliffordNetUNet, create_cliffordnet_depth
 from .conditional_denoiser import CliffordNetConditionalDenoiser
@@ -8,6 +9,7 @@ from .confidence_denoiser import CliffordNetConfidenceDenoiser
 __all__ = [
     "CliffordNet",
     "CliffordNetLM",
+    "CliffordNetLMRouting",
     "CliffordCLIP",
     "CliffordNetUNet",
     "create_cliffordnet_depth",
