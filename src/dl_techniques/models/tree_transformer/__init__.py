5bd0bd76a1e350fa6fa2f02f47ca067f40040cfe
Nikolas Markou <nikolasmarkou@gmail.com>
2026-05-11 12:01:21 +0300

    [iter-1/step-4] tree_transformer: trim package __init__ to 3-name public API

--- src/dl_techniques/models/tree_transformer/__init__.py
+++ src/dl_techniques/models/tree_transformer/__init__.py
@@ -1,17 +1,11 @@
 from dl_techniques.models.tree_transformer.model import (
     TreeTransformer,
-    TreeTransformerBlock,
-    GroupAttention,
-    TreeMHA,
-    PositionalEncoding,
+    create_tree_transformer,
     create_tree_transformer_with_head,
 )
 
 __all__ = [
     "TreeTransformer",
-    "TreeTransformerBlock",
-    "GroupAttention",
-    "TreeMHA",
-    "PositionalEncoding",
+    "create_tree_transformer",
     "create_tree_transformer_with_head",
 ]
