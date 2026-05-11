127c5cdac0f9118946b7d1980c685f4a08bb66a4
Nikolas Markou <nikolasmarkou@gmail.com>
2026-05-11 08:11:27 +0300

    [iter-1/step-5] tree_transformer: re-export public API from __init__.py

---
+++ src/dl_techniques/models/tree_transformer/__init__.py
@@ -0,0 +1,17 @@
+from dl_techniques.models.tree_transformer.model import (
+    TreeTransformer,
+    TreeTransformerBlock,
+    GroupAttention,
+    TreeMHA,
+    PositionalEncoding,
+    create_tree_transformer_with_head,
+)
+
+__all__ = [
+    "TreeTransformer",
+    "TreeTransformerBlock",
+    "GroupAttention",
+    "TreeMHA",
+    "PositionalEncoding",
+    "create_tree_transformer_with_head",
+]
