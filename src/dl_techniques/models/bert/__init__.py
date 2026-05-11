a299705b9f2cc287824f7708d06d2c6f42a618c1
Nikolas Markou <nikolasmarkou@gmail.com>
2026-05-11 13:50:31 +0300

    [iter-1/step-4] bert: trim package __init__ to 3-name public API

--- src/dl_techniques/models/bert/__init__.py
+++ src/dl_techniques/models/bert/__init__.py
@@ -1,5 +1,3 @@
-from .bert import BERT, create_bert_with_head, create_nlp_head
+from .bert import BERT, create_bert, create_bert_with_head
 
-__all__ = [
-    "BERT", "create_bert_with_head", "create_nlp_head"
-]
+__all__ = ["BERT", "create_bert", "create_bert_with_head"]
