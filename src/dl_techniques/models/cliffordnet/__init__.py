a97990920af068fda499b207c6c5d311e6f9f10d
Nikolas Markou <nikolasmarkou@gmail.com>
2026-05-12 17:10:46 +0300

    [iter-1/step-5] cliffordnet/__init__: re-export embedding model surface

--- src/dl_techniques/models/cliffordnet/__init__.py
+++ src/dl_techniques/models/cliffordnet/__init__.py
@@ -1,10 +1,18 @@
 from .model import CliffordNet, create_cliffordnet
 from .clip import CliffordCLIP
 from .lm_routing import CliffordNetLMRouting
+from .embedding_unet import (
+    CliffordNetEmbedding,
+    create_cliffordnet_embedding,
+    create_cliffordnet_embedding_with_head,
+)
 
 __all__ = [
     "CliffordNet",
     "create_cliffordnet",
     "CliffordCLIP",
     "CliffordNetLMRouting",
+    "CliffordNetEmbedding",
+    "create_cliffordnet_embedding",
+    "create_cliffordnet_embedding_with_head",
 ]
