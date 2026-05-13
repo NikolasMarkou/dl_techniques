8e59d4791fb475889ce3e9b26b64e0827dbd8d7d
Nikolas Markou <nikolasmarkou@gmail.com>
2026-05-13 07:58:14 +0300

    [iter-1/step-3] export KANLinear and TverskyProjectionLayer from ffn/__init__.py

--- src/dl_techniques/layers/ffn/__init__.py
+++ src/dl_techniques/layers/ffn/__init__.py
@@ -7,6 +7,8 @@ from .residual_block import ResidualBlock
 from .swin_mlp import SwinMLP
 from .logic_ffn import LogicFFN
 from .counting_ffn import CountingFFN
+from .kan_linear import KANLinear
+from .tversky_projection import TverskyProjectionLayer
 
 from .factory import (
     FFNType,
@@ -28,6 +30,8 @@ __all__ = [
     GeGLUFFN,
     ResidualBlock,
     CountingFFN,
+    KANLinear,
+    TverskyProjectionLayer,
     FFNType,
     create_ffn_layer,
     create_ffn_from_config,
