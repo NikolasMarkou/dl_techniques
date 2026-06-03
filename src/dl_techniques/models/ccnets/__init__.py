444a1d28c83a17ec2fc0156d8d67a5a6474a265b
Nikolas Markou <nikolasmarkou@gmail.com>
2026-06-03 16:02:50 +0300

    [iter-1/step-5] ccnets: export migrated blocks + architectures + factories from package __init__

--- src/dl_techniques/models/ccnets/__init__.py
+++ src/dl_techniques/models/ccnets/__init__.py
@@ -1,13 +1,59 @@
+# --- Framework: config, trainer, orchestrators, utils -----------------------
 from .base import CCNetConfig
 from .trainer import CCNetTrainer
 from .utils import EarlyStoppingCallback, wrap_keras_model
 from .orchestrators import CCNetOrchestrator, SequentialCCNetOrchestrator
 
+# --- Shared building blocks --------------------------------------------------
+from .blocks import FiLMLayer, ConvBlock, DenseBlock
+
+# --- Concrete task architectures + factories ---------------------------------
+from .architectures import (
+    MNISTExplainer,
+    MNISTReasoner,
+    MNISTProducer,
+    create_mnist_ccnet,
+    Cifar100Explainer,
+    Cifar100Reasoner,
+    Cifar100Producer,
+    create_cifar100_ccnet,
+    HybridCCNetOrchestrator,
+    SentimentExplainer,
+    SentimentReasoner,
+    SentimentProducer,
+    ARSentimentProducer,
+    TextCCNetOrchestrator,
+    ARTextCCNetOrchestrator,
+    create_text_ccnet,
+)
+
 __all__ = [
+    # framework
     "CCNetConfig",
     "CCNetTrainer",
     "CCNetOrchestrator",
     "SequentialCCNetOrchestrator",
     "EarlyStoppingCallback",
     "wrap_keras_model",
+    # blocks
+    "FiLMLayer",
+    "ConvBlock",
+    "DenseBlock",
+    # architectures + factories
+    "MNISTExplainer",
+    "MNISTReasoner",
+    "MNISTProducer",
+    "create_mnist_ccnet",
+    "Cifar100Explainer",
+    "Cifar100Reasoner",
+    "Cifar100Producer",
+    "create_cifar100_ccnet",
+    "HybridCCNetOrchestrator",
+    "SentimentExplainer",
+    "SentimentReasoner",
+    "SentimentProducer",
+    "ARSentimentProducer",
+    "TextCCNetOrchestrator",
+    "ARTextCCNetOrchestrator",
+    "create_text_ccnet",
 ]
