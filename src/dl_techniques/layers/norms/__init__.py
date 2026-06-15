015847e4f9f802959b5c6facb1347077bd29765e
Nikolas Markou <nikolasmarkou@gmail.com>
2026-06-15 09:29:05 +0300

    [iter-1/step-7] norms __init__: C2 fix broken package API - import all 14 classes (was missing AdaptiveBandRMS, GlobalResponseNormalization, DynamicTanh, ZeroCenteredRMSNorm, ZeroCenteredBandRMSNorm) + factory symbols; __all__ now a list of STRING names (was class objects)

--- src/dl_techniques/layers/norms/__init__.py
+++ src/dl_techniques/layers/norms/__init__.py
@@ -1,25 +1,43 @@
-from .band_rms import BandRMS
-from .band_logit_norm import BandLogitNorm
-from .logit_norm import LogitNorm
 from .rms_norm import RMSNorm
-from .max_logit_norm import MaxLogitNorm, DecoupledMaxLogit, DMLPlus
+from .zero_centered_rms_norm import ZeroCenteredRMSNorm
+from .band_rms import BandRMS
+from .adaptive_band_rms import AdaptiveBandRMS
+from .band_logit_norm import BandLogitNorm
+from .zero_centered_band_rms_norm import ZeroCenteredBandRMSNorm
 from .zero_centered_adaptive_band_rms_norm import ZeroCenteredAdaptiveBandRMS
+from .logit_norm import LogitNorm
+from .max_logit_norm import MaxLogitNorm, DecoupledMaxLogit, DMLPlus
+from .global_response_norm import GlobalResponseNormalization
+from .dynamic_tanh import DynamicTanh
 from .polar_weight_norm import PolarWeightNorm, polar_encode, polar_decode
-from .factory import create_normalization_layer,create_normalization_from_config, NormalizationType
-
-__all__ = [
-    BandRMS,
-    BandLogitNorm,
-    LogitNorm,
-    RMSNorm,
-    MaxLogitNorm,
-    DecoupledMaxLogit,
-    DMLPlus,
-    ZeroCenteredAdaptiveBandRMS,
-    PolarWeightNorm,
-    polar_encode,
-    polar_decode,
+from .factory import (
     create_normalization_layer,
     create_normalization_from_config,
-    NormalizationType
+    get_normalization_info,
+    validate_normalization_config,
+    NormalizationType,
+)
+
+__all__ = [
+    "RMSNorm",
+    "ZeroCenteredRMSNorm",
+    "BandRMS",
+    "AdaptiveBandRMS",
+    "BandLogitNorm",
+    "ZeroCenteredBandRMSNorm",
+    "ZeroCenteredAdaptiveBandRMS",
+    "LogitNorm",
+    "MaxLogitNorm",
+    "DecoupledMaxLogit",
+    "DMLPlus",
+    "GlobalResponseNormalization",
+    "DynamicTanh",
+    "PolarWeightNorm",
+    "polar_encode",
+    "polar_decode",
+    "create_normalization_layer",
+    "create_normalization_from_config",
+    "get_normalization_info",
+    "validate_normalization_config",
+    "NormalizationType",
 ]
