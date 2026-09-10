b6b30c466fbca1152824315b0fa59cc7fb21fdab
Nikolas Markou <nikolasmarkou@gmail.com>
2026-09-10 12:45:06 +0300

    [layers] cleaning up

--- src/dl_techniques/layers/tabular/tabm_mlp_block.py
+++ src/dl_techniques/layers/tabular/tabm_mlp_block.py
@@ -360,3 +360,5 @@ class TabMMLPBlock(keras.layers.Layer):
             "bias_regularizer": keras.regularizers.serialize(self.bias_regularizer),
         })
         return config
+
+# ---------------------------------------------------------------------
